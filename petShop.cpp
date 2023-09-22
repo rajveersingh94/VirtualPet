48fc64dd9e01a8ef7886ce5f17a2627331c099bb
Rajveer Singh <rasing@cs.csis.work>
2023-09-22 12:31:11 -0500

    Change the main driver

--- petShop.cpp
+++ petShop.cpp
@@ -1,4 +1,8 @@
+
 #include "VirtualPet.h"
+#include "Digimon.h"
+#include "Pikachu.h"
+#include "Gatomon.h"
 #include "Pokemon.h"
 
 int main() {
@@ -21,6 +25,42 @@ int main() {
 	cout << "Hunger: " << pokemanz.getHunger() << endl;
 	cout << "Health: " << pokemanz.getHealth() << endl;
 	cout << "Species: " << pokemanz.getSpecies() << endl;
+// Create a Digimon instance
+    Digimon digimon("Agumon", 80, 30, "Fire");
+
+    // Call Digimon's unique member function
+    digimon.useSpecialAttack();
+
+    // Display Digimon's information
+    cout << "Name: " << digimon.getName() << endl;
+    cout << "Element: " << digimon.getElement() << endl;
+    cout << "Happiness: " << digimon.getHappiness() << endl;
+    cout << "Hunger: " << digimon.getHunger() << endl;
+
+    // Create a Pikachu instance
+    Pikachu pikachu("Sparky", "Electric", 100, 50);
+
+    // Call Pikachu's unique member function
+    pikachu.useElectricShock();
+
+    // Display Pikachu's information
+    cout << "Name: " << pikachu.getName() << endl;
+    cout << "Species: " << pikachu.getSpecies() << endl;
+    cout << "Health: " << pikachu.getHealth() << endl;
+    cout << "Electric Power: " << pikachu.getElectricPower() << endl;
+
+    // Create a Gatomon instance
+    Gatomon gatomon("Whiskers", "Light", 90, 40, 60);
+
+    // Call Gatomon's unique member function
+    gatomon.useCatClawAttack();
+
+    // Display Gatomon's information
+    cout << "Name: " << gatomon.getName() << endl;
+    cout << "Element: " << gatomon.getElement() << endl;
+    cout << "Happiness: " << gatomon.getHappiness() << endl;
+    cout << "Hunger: " << gatomon.getHunger() << endl;
+    cout << "Cat Claw Sharpness: " << gatomon.getCatClawSharpness() << endl;
 
 	return 0;
 }

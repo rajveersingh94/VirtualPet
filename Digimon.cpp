7e95b35bd54f9b3c5615d66b0d5ebcbf55fb6565
Rajveer Singh <rasing@cs.csis.work>
2023-09-22 12:08:56 -0500

    corrected the constructor

--- Digimon.cpp
+++ Digimon.cpp
@@ -1,7 +1,7 @@
 #include "Digimon.h"
 
 Digimon::Digimon(const string& name, int happiness, int hunger, const string& element)
-    : VirtualPet(name, happiness, hunger), element(element) {}
+    : VirtualPet(name), element(element) {}
 
 void Digimon::useSpecialAttack() {
     cout << getName() << " uses Special Attack!" << endl;
